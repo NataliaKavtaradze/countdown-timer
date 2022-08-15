e99df12175550365cb0009349343fe2573ca9bd1
Natalia Kavtaradze <nataliakavtaradze777@gmail.com>
2022-08-15 11:10:35 +0400

    changed countdoun date in js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("aug 30, 2022 00:00:00").getTime();
+let countDownDate = new Date("dec 31, 2022 00:00:00").getTime();
 // Update the count down every 1 second
 let x = setInterval(function () {
   // Get today's date and time
